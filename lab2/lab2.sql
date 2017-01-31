edfb8da57bfb1effeb84f6db16ed6b3aa44c5302
Niklas <nicko_carlsson@hotmail.com>
2017-01-31 13:52:06 +0100

    hej

---
+++ lab2/lab2.sql
@@ -0,0 +1,58 @@
+PRAGMA foreign_keys=OFF;
+
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS theaters;
+DROP TABLE IF EXISTS movies;
+DROP TABLE IF EXISTS performances;
+DROP TABLE IF EXISTS reservations;
+
+PRAGMA foreign_keys=ON;
+
+CREATE TABLE users {
+    username    VARCHAR(32) NOT NULL,
+    address     VARCHAR(32),
+    name        VARCHAR(32) NOT NULL,
+    tel_nbr     VARCHAR(16) NOT NULL,
+    PRIMARY KEY (username)
+};
+
+CREATE TABLE theaters {
+    name        VARCHAR(32) NOT NULL,
+    seats       INT         NOT NULL,
+    PRIMARY KEY (name, seats)
+};
+
+CREATE TABLE movies {
+    name        VARCHAR(32) NOT NULL,
+    PRIMARY KEY (name)
+};
+
+CREATE TABLE performances {
+    date        DATE NOT NULL,
+    movie_name  VARCHAR(32) NOT NULL,
+    theater_name    VARCHAR(32) NOT NULL,
+    theater_seats   INT NOT NULL,
+    PRIMARY KEY (date),
+    FOREIGN KEY (movie_name) references movies(name),
+    FOREIGN KEY (theater_name) references theaters(name),
+    FOREIGN KEY (theater_seats) references theaters(seats)
+};
+
+CREATE TABLE reservations {
+    reservation_number  INT NOT NULL,
+    movie_name          VARCHAR(32) NOT NULL,
+    date                DATE NOT NULL,
+    PRIMARY KEY (reservation_number),
+    FOREIGN KEY (movie_name) references movies(name),
+    FOREIGN KEY (date) references performances(date)
+};
+
+INSERT INTO users           VALUES('jacka', 'Lund', 'NickoYeah', '112');
+
+INSERT INTO theaters        VALUES('SF', 2);
+
+INSERT INTO movies          VALUES('Kung Fu Hustle');
+
+INSERT INTO performances    VALUES('2017-02-01', 'Kung Fu Hustle', 'SF', 2);
+
+INSERT INTO reservations    VALUES(NULL, 'Kung Fu Hustle', '2017-02-01');
